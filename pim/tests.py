e1f6e0af0529fcd833980bd4dd3db4dc5f29877a
Jess Easter <jeaster2@my.waketech.edu>
2021-02-09 20:32:41 -0500

    Fix github action

---
+++ pim/tests.py
@@ -0,0 +1,15 @@
+import unittest
+from django.test import Client
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('/admin/')
+
+        # Check that the response is a 302 redirect to login
+        self.assertEqual(response.status_code, 302)
+
