8819cda830c49101b5f2d9287da9e6a5f6f09e6e
imercadovazquez <imercadovazquez@my.waketech.edu>
2021-02-17 14:07:03 -0500

    added search page

--- prod_inventory_app/filters.py
+++ prod_inventory_app/filters.py
@@ -6,3 +6,16 @@ class ProductFilter(django_filters.FilterSet):
     class Meta:
         model = Product
         fields = ['name']
+
+
+class RestockFilter(django_filters.FilterSet):
+    class Meta:
+        model = Restock
+        fields = ['product', 'order_date']
+		
+		
+		
+class SaleFilter(django_filters.FilterSet):
+    class Meta:
+        model = Sale
+        fields = ['product', 'sale_date']
