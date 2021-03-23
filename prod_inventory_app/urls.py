31cdf371ddd2ace706d4a822b31bef941a4f1e3c
imercadovazquez <imercadovazquez@my.waketech.edu>
2021-03-22 20:49:03 -0400

    fixed spacing in urls.py

--- prod_inventory_app/urls.py
+++ prod_inventory_app/urls.py
@@ -18,4 +18,4 @@ urlpatterns = [
     path('add_to_stock_csv/', views.add_to_stock_csv, name='add_to_stock'),
     path('reports/', views.reports, name='reports'),
     path('remove_item/<str:pk>/', views.remove_item, name='remove_item'),
-    ]
+]
